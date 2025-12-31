a5d1d00c773f53cc6931abdaec14b42d9446896d
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2025-12-31 19:55:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -0,0 +1,89 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+  
+     void traversal(Node* root, vector<vector<int>> &positive, vector<vector<int>> &negative){
+        if(root == nullptr) return;
+        queue<Node*> q;
+        queue<int> idx;
+        q.push(root);
+        idx.push(0);
+        while(!q.empty()){
+            Node* temp = q.front();
+            int p = idx.front();
+            q.pop();
+            idx.pop();
+            if(temp->left){
+                q.push(temp->left);
+                idx.push(p-1);
+            }
+            if(temp->right){
+                q.push(temp->right);
+                idx.push(p+1);
+            }
+            if(p >= 0){
+                positive[p].push_back(temp->data);
+            }else{
+                negative[-p].push_back(temp->data);
+            }
+        }
+    }
+    void position(Node* root, int p, int &l, int &r){
+        if(root == nullptr) return;
+        
+        l = min(p,l);
+        r = max(p,r);
+        if(root->left) position(root->left, p-1, l, r);
+        if(root->right) position(root->right, p+1, l, r);
+    }
+    vector<int> topView(Node *root) {
+        // code here
+        if(root == nullptr) return {};
+        
+        int l = 0;
+        int r = 0;
+        
+        position(root, 0, l, r);
+        
+        vector<int> res;
+        
+        vector<vector<int>> positive(r+1, vector<int>());
+        vector<vector<int>> negative(abs(l)+1, vector<int>());
+        
+        traversal(root, positive, negative);
+        
+        for(int i=negative.size()-1; i>0; i--){
+            res.push_back(negative[i][0]);
+        }
+        for(int i=0; i<positive.size(); i++){
+            res.push_back(positive[i][0]);
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
