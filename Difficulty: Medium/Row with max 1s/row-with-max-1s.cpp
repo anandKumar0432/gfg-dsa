25c289b71ffa246a4665667372157722206c3562
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-03-02 23:58:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -1,32 +1,30 @@
 // User function template for C++
 class Solution {
   public:
+  
+    int countOnes(int row, int col, vector<vector<int>> &arr){
+        
+        int count = 0;
+        for(int i = 0; i<col; i++){
+            if(arr[row][i] == 1) count++;
+        }
+        
+        return count;
+    }
     int rowWithMax1s(vector<vector<int>> &arr) {
         // code here
         int row = arr.size();
-        int col = arr[0].size();
-        int result = -1;
-        int maxi = INT_MIN;
-        for(int i=0; i<row; i++){
-            int start = 0;
-            int end = col-1;
-            int nOf1 = -1;
-            while(start <= end){
-                int mid = start + (end - start)/2;
-                if(arr[i][mid] == 1){
-                    nOf1 = col - mid;
-                    end = mid-1;
-                }else{
-                    start = mid + 1;
-                }
+        int maxi = 0;
+        int maxIndex = -1;
+        
+        for(int i = 0; i < row; i++){
+            int col = arr[i].size();
+            if(countOnes(i, col, arr) > maxi){
+                maxi = countOnes(i, col, arr);
+                maxIndex = i;
             }
-            if(nOf1 > maxi){
-                result = i;
-                maxi = nOf1;
-            }
-            
         }
         
-        return result;
+        return maxIndex;
     }
 };
