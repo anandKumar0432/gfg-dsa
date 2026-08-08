9191919c38c31bcf59e9f263d10f82fc9d2c147d
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-08-08 20:25:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Valid Word with All Prefixes/longest-valid-word-with-all-prefixes.cpp
@@ -0,0 +1,92 @@
+struct Node{
+    Node* links[26];
+    bool flag = false;
+    
+    bool containsKey(char ch){
+        return links[ch - 'a'] != NULL;
+    }
+    
+    Node* get(char ch){
+        return links[ch - 'a'];
+    }
+    
+    void put(char ch, Node* node){
+        links[ch - 'a'] = node;
+        
+    }
+    
+    bool isEnd(){
+        return flag;
+    }
+    
+    void setEnd(){
+        flag = true;
+    }
+};
+
+class Trie{
+    private: Node* root;
+    
+    public: 
+    
+    Trie(){
+        root = new Node();
+    }
+    
+    void insert(string word){
+        Node* node = root;
+        
+        for(int i=0; i<word.length(); i++){
+            if(!node->containsKey(word[i])){
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        
+        node->setEnd();
+    }
+    
+    bool checkIfPrefixExist(string word){
+        Node* node = root;
+        
+        for(int i=0; i<word.length(); i++){
+            if(node->containsKey(word[i])){
+                node = node->get(word[i]);
+                if(node->isEnd() == false) return false;
+            } else return false;
+        }
+        
+        return true;
+    }
+};
+
+class Solution {
+  public:    
+    string longestValidWord(vector<string>& words) {
+        // code here
+        Trie* tr = new Trie();
+        
+        for(int i=0; i<words.size(); i++){
+            tr->insert(words[i]);
+        }
+        
+        string longest = "";
+        
+        for(auto &it : words){
+            if(tr->checkIfPrefixExist(it)){
+                if(it.length() > longest.length()) longest = it;
+                
+                if(it.length() == longest.length() && it < longest) longest = it;
+            }
+            
+        }
+        
+        return longest;
+    }
+};
+
+
+
+
+
+
